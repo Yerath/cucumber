cf30d82684d591b58c082a99e1c71478c10824b8
Roelant Ossewaarde <roelanto@gmail.com>
2016-04-13 08:39:31 +0200

    Fixed typo's, minor things.

--- features/step_definitions/numerals.rb
+++ features/step_definitions/numerals.rb
@@ -1,9 +1,16 @@
+
+
 Given(/^the input "(.*?)"$/) do |arg1|
   @input = arg1
 end
 
 When(/^the converter is run$/) do
   @output = `javac Main.java && java Main {@input}`
+  raise('Command failed!') unless $?.success?
+end
+
+When(/^the input is (\d+)$/) do |arg1|
+  @output = `javac Main.java && java Main arg1`
   raise('Command failed!') unless $?.success?
 end
 
